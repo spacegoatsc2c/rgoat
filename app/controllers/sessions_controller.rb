65fb040e81606f3b20885965f58dc2c51098ab5f
Joseph Utecht <joseph.utecht@gmail.com>
2018-09-03 23:02:13 -0500

    need better way to do this

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ class SessionsController < ApplicationController
     bnet = Battlenet.new(locale: "en_us", api_key: ENV['BNET_API'])
     users_characters = bnet.characters(access_token: token)
     users_characters['characters'].each do | character |
-      if character.fetch('guild') == 'Space Goats CoastToCoast' &&
+      if character.fetch('guild', nil) == 'Space Goats CoastToCoast' &&
          character.fetch('realm') == 'Whisperwind'
         c = Character.find_or_create_by(name: character['name'],
                                         portrait: bnet.character_image(character: character),
