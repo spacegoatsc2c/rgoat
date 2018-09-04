c04209536a92333902e98b691c80d805c6c8ce90
Joseph Utecht <joseph.utecht@gmail.com>
2018-09-03 22:39:11 -0500

    multiline comments?

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,15 +4,13 @@ class SessionsController < ApplicationController
   def create
     puts request.env['omniauth.auth']
     render plain: request.env['omniauth.auth'].inspect
-    =begin
-    bnet = Battlenet.new(locale: "en_us", api_key: ENV['BNET_API'])
-    guild = bnet.guild(realm: "Whisperwind", name: "Space Goats CoastToCoast")
-    classes = bnet.classes
-    class_names = {}
-    classes['classes'].each do | c |
-      class_names[c['id']] = c['name']
-    end
-    bnet.characters(access_token: request.env['onmiauth.auth'].credentials.token)
-    =end
+    # bnet = Battlenet.new(locale: "en_us", api_key: ENV['BNET_API'])
+    # guild = bnet.guild(realm: "Whisperwind", name: "Space Goats CoastToCoast")
+    # classes = bnet.classes
+    # class_names = {}
+    # classes['classes'].each do | c |
+    #   class_names[c['id']] = c['name']
+    # end
+    # bnet.characters(access_token: request.env['onmiauth.auth'].credentials.token)
   end
 end
